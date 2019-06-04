f1a5e1d1adddd5f42e42e3b74d0bf8090fa75a79
giacomocherubini <giacomo.cherubini1@gmail.com>
2019-06-04 09:37:00 +0200

    inserimento nuovo todo

--- main.js
+++ main.js
@@ -2,16 +2,41 @@ $(document).ready(function()  {
 
   var url_base = 'http://157.230.17.132:3004/todos';
 
-  $. ajax({
-    'url': url_base,
-    'method': 'GET',
-    'success': function(data) {
-      for (var i = 0; i < data.length; i++) {
-        $('#azioni_da_fare').append('<li>'+ data[i] + '</li>');
+  stampa_todo();
+
+  $('#nuova_azione').click(function() {
+    var new_action = $('#testo_azione').val();
+    // resetto il l'input
+    $('#testo_azione').val('');
+    $. ajax({
+      'url': url_base,
+      'method': 'POST',
+      'data': {
+      'text': new_action
+      },
+      'success': function() {
+          stampa_todo();
+      },
+      'error': function() {
+        alert('error');
       }
-    },
-    'error': function() {
-      alert('error');
-    }
+    });
   });
+
+  function stampa_todo(data) {
+    $('#azioni_da_fare').html('');
+    $. ajax({
+      'url': url_base,
+      'method': 'GET',
+      'success': function(data) {
+        for (var i = 0; i < data.length; i++) {
+          $('#azioni_da_fare').append('<li>'+ data[i].text + '</li>');
+        }
+      },
+      'error': function() {
+        alert('error');
+      }
+    });
+  }
+
 });
